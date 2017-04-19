ebcc1eb6e09f2b0eab9026edb6e97e0ffb5d2001
Shadow-Death <Polarfuchs123@gmx.de>
2017-04-20 00:46:24 +0200

    added comment

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -54,7 +54,7 @@ public class Main {
         commandHandler.commands.put("say", new cmdSay());
         commandHandler.commands.put("clear", new cmdClear());
 
-        //te
+        //test
 
         try {
             JDA jda = builder.buildBlocking();
