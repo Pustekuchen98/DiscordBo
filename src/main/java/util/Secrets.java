99c08bc5648b93a3e24a94f6f5fbbcaa72f9c6c5
Pustekuchen98 <Polarfuchs123@gmx.de>
2017-04-21 18:25:50 +0200

    Delete Secrets.java

--- src/main/java/util/Secrets.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-/**
- * Created by ich1 on 17.04.2017.
- */
-public class Secrets {
-
-    public static String TOKEN = "MzAzNjI0ODY3NjU4MzM0MjEx.C9a0Pw.mt8vHdGEw05y0yDOl5IkXZc_aus";
-}
